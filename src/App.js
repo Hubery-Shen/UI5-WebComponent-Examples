e949d54500fb6f350701f2813446cde3b051befc
Hubery Shen <hubery.shen@sap.com>
2020-08-20 09:53:38 +0800

    finish demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import React Framework
+import React, { useEffect, useRef } from "react";
+
+// Import UI5 libs
+import "react-app-polyfill/stable";
+import "@ui5/webcomponents/dist/Assets.js";
+import "@ui5/webcomponents-icons/dist/Assets.js";
+
+// Import Tested Component
+import "@ui5/webcomponents/dist/Popover.js";
+
+// Import CSS
+import "./App.css";
 
 function App() {
+  const targetRef = useRef(null);
+  const popoverRef = useRef(null);
+  useEffect(() => {
+    popoverRef.current && popoverRef.current.openBy(targetRef.current);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div ref={targetRef} id="target" style={{ width: "100%", height: "100px", background: "lightblue" }} />
+        <div className="snippet">
+          <ui5-popover ref={popoverRef} horizontal-align="Left" id="hello-popover" header-text="Newsletter subscription">
+            <div className="popover-content">
+              <div className="flex-column">
+                <ui5-label htmlFor="emailInput">Email:</ui5-label>
+                <ui5-input id="emailInput" className="samples-margin-top" placeholder="Enter Email" />
+              </div>
+            </div>
+            <div slot="footer" className="popover-footer">
+              <div style={{ flex: 1 }} />
+              <ui5-button id="closePopoverButton" design="Emphasized">
+                Subscribe
+              </ui5-button>
+            </div>
+          </ui5-popover>
+        </div>
+      </div>
   );
 }
 
